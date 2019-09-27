b6dc256811c6d9db8cbc0372964101d9333b6a91
vinod <borse002@gmail.com>
2019-09-28 02:21:52 +0530

    Add basic spring boot application

---
+++ src/main/java/com/vinu/simplesprinbootapp/response/GenericResponse.java
@@ -0,0 +1,25 @@
+package com.vinu.simplesprinbootapp.response;
+
+/**
+ * @author Vinod Borse
+ * 19-Sep-2019
+ */
+public class GenericResponse {
+	private String response;
+
+	public GenericResponse() {
+		super();
+	}
+  
+	public GenericResponse(String response) {
+		this.response = response;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+}
---
+++ src/test/java/com/vinu/simplesprinbootapp/SimpleSprinBootAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vinu.simplesprinbootapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleSprinBootAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
